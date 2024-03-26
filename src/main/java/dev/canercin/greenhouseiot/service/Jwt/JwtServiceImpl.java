5050ee1a8fdb4ff39aecc9be702122d71d64708b
canercin <canercincin34@gmail.com>
2024-03-26 10:49:36 +0300

    change secret key

--- src/main/java/dev/canercin/greenhouseiot/service/Jwt/JwtServiceImpl.java
+++ src/main/java/dev/canercin/greenhouseiot/service/Jwt/JwtServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService {
-    private static final String secretKey = "greenhouse";
+    private static final String secretKey = "4vWEJ7NgKtm97QktGjOZ925LNZxoAX7F9NiEaT0ABj1DUVb9Tb5PuMbjspmDwjEIAad330F9nG9SQclkgGy/hH9Cynt1GzJ2R0ODxv1mJ8ZQsSfNAb1E0s4Liivsn3kYyJEQZno70fd3Kxi0STv/IYmxU+A2jTPUcOGQn3qU707+W1bwlc+WAyiQt3d3znTYVRwMxBbT/6NXVvQFM9cy1sPWIwyAu0Yz3KW5nXpAtLnWCAMT0FhjiKCO20IqxO0Sugoz2PHwuwDmnDexg8kf0M06o90tM6XS2IICkGs9BnckE3MXg8fWOTpjZcG5lBS/0amsy84Xew5ThY7cySPM5wSL2jNqcBDGeUJMSEupMXU=";
     @Override
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
